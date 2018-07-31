2b0af6564ca87a056b438a0493256bdcc1cf2658
Marissa Zhong <marissazhong@Marissas-MacBook-Pro.local>
2018-07-31 14:45:38 -0400

    start new recipe form view and create migration to add time columns to recipes table

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,3 +1,29 @@
 class RecipesController < ApplicationController
+    before_action :set_recipe, only: [:show, :edit, :update, :destroy]
+
+    # /recipes index page showing user's recipes and all recipes
+    def index
+        @recipes = Recipe.all
+    end
+
+    def show
+    end
+
+    def new
+        @recipe = Recipe.new
+    end
+
+    def edit
+    end
+
+    private
+
+        def set_recipe
+            @recipe = Recipe.find(params[:id])
+        end
+
+        def recipe_params
+            params.require(:attraction).permit(:name)
+        end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180730142646) do
+ActiveRecord::Schema.define(version: 20180731184405) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 20180730142646) do
     t.text "instructions"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "prep_time"
+    t.integer "cook_time"
   end
 
   create_table "users", force: :cascade do |t|
