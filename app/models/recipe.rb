947122e1aa20210f445f88f2823dee86b8e6dd01
Marissa Zhong <marissazhong@Marissas-MacBook-Pro.local>
2018-08-02 15:10:36 -0400

    add recipes indices to user show page

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,4 +4,12 @@ class Recipe < ActiveRecord::Base
     belongs_to :user
     accepts_nested_attributes_for :recipe_ingredients
     validates :name, presence: true
+
+    def recipe_ingredients_attributes=(recipe_ingredients_attributes)
+        recipe_ingredients_attributes.values.each do |recipe_ingredients_attribute|
+            ingredient = Ingredient.find_or_create_by(name: recipe_ingredients_attribute[:input_name].downcase)
+            recipe_ingredient = RecipeIngredient.create(recipe_ingredients_attribute)
+            # self.recipe_ingredients << recipe_ingredient
+        end
+    end
 end
