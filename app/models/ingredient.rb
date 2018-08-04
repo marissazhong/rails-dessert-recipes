8fd95fd1ec40eb18873da60043a665b51f2ec98f
Marissa Zhong <marissazhong@Marissas-MacBook-Pro.local>
2018-08-03 22:52:01 -0400

    update spec.md file

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -3,6 +3,7 @@ class Ingredient < ActiveRecord::Base
     has_many :recipes, :through => :ingredients_recipe
     has_many :ingredients_pantries
     has_many :pantries, through: :ingredients_pantries
+    validates :name, presence: true
     def titleize
         name.titleize
     end
